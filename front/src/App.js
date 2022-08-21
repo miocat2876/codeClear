5609f5325c9c2512afb5411706f52a5964a22655
miocat <kkm1640@naver.com>
2022-08-22 08:18:03 +0900

    훅 테스트

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Main from './pages/Main';
 import Header from "./layout/Header";
@@ -9,8 +9,25 @@ import Academy from "./pages/Academy";
 import FreeBoardDetail from "./pages/FreeBoardDetail";
 import FreeBoardWrite from './pages/FreeBoardWrite';
 import FreeBoardList from './pages/FreeBoardList';
+import axios from "axios";
 
 function App() {
+
+	const myKey = 'yxcDuG7h26fepYeGSP2foZv5FjP0wZC6';
+	const sdate = '20220701';
+	const edate = '20230101';
+	const hrdList = 'https://www.hrd.go.kr/hrdp/api/apipo/APIPO0101T.do?returnType=XML&pageSize=10&' +
+		'authKey='+myKey+
+		'&sort=ASC&crseTracseSe=C0055&outType=1&srchTraStDt=20220701&pageNum=1&sortCol=TR_STT_DT&srchTraEndDt=20230101&srchTraPattern=N1&srchPart=-99&apiRequstPageUrlAdres=/jsp/HRDP/HRDPO00/HRDPOA60/HRDPOA60_1.jsp&apiRequstIp=14.40.58.136';
+	useEffect(()=>{
+		axios({
+			method:'get',
+			url:hrdList
+		}).then(response => {console(response.data)}
+		)
+	})
+
+
 
 	const browserSize = useSelector(state => state.browserSize);
 	const appStyled = {
